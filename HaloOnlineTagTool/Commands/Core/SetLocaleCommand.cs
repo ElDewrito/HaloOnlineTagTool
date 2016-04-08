8876dae10baae914517598c2ce6d4ba3cb1292c6
maximumgame <nickemerich@gmail.com>
2016-04-07 23:42:42 -0400

    Set a locale
    Command to change the locale
    Those that use comma decimal separation can change the tag tool to use
    comma instead of periods.

---
+++ HaloOnlineTagTool/Commands/Core/SetLocaleCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace HaloOnlineTagTool.Commands.Core
+{
+    class SetLocaleCommand : Command
+    {
+        public SetLocaleCommand()
+            : base(CommandFlags.Inherit,
+                  "SetLocale",
+                  "Changes the parsing locale of numbers to the specified locale.",
+                  "SetLocale <locale>",
+        {
+        }
+
+        public override bool Execute(List<string> args)
+        {
+            if (args.Count < 1)
+                return false;
+            CultureInfo ci;
+            try
+            {
+                ci = CultureInfo.GetCultureInfo(args[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            CultureInfo.DefaultThreadCurrentCulture = ci;
+            return true;
+        }
+    }
+}
